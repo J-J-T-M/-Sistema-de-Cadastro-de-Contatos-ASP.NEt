f63e6b71c6b5f64414b338db8de843174d46eae0
J-J-T-M <jonatham.junior.18@gmail.com>
2023-05-11 12:39:21 -0400

    create ContatoController

---
+++ FirstAppASP.NET/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAppASP.NET.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
