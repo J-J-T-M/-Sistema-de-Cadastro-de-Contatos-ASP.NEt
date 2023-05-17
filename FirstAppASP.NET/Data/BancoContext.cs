528028e150f08ad6a07ffe5faae265c67e7b161d
J-J-T-M <jonatham.junior.18@gmail.com>
2023-05-17 17:34:18 -0400

    create BancoContext

---
+++ FirstAppASP.NET/Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using FirstAppASP.NET.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAppASP.NET.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext>options) : base(options)
+        {
+        }
+
+        public DbSet<ContactModel> Contacts { get; set; }
+    }
+}
