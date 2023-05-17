7f2a89bb4172f7131d4101714628804a4c4ed80f
J-J-T-M <jonatham.junior.18@gmail.com>
2023-05-17 17:38:09 -0400

    adding AddDbContext

--- FirstAppASP.NET/Program.cs
+++ FirstAppASP.NET/Program.cs
@@ -1,3 +1,6 @@
+using FirstAppASP.NET.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace FirstAppASP.NET
 {
     public class Program
@@ -8,6 +11,11 @@ namespace FirstAppASP.NET
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<BancoContext>( options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase"));
+            });
 
             var app = builder.Build();
 
