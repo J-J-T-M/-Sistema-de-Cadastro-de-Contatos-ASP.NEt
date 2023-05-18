50482e30589e56edff1107412fc05ebb6f94a5ac
J-J-T-M <jonatham.junior.18@gmail.com>
2023-05-18 10:44:23 -0400

    adding List GetAll

---
+++ FirstAppASP.NET/Repository/IContactRepository.cs
@@ -0,0 +1,10 @@
+﻿using FirstAppASP.NET.Models;
+
+namespace FirstAppASP.NET.Repository
+{
+    public interface IContactRepository
+    {
+        List<ContactModel> GetAll();
+        ContactModel Adding(ContactModel contact);
+    }
+}
