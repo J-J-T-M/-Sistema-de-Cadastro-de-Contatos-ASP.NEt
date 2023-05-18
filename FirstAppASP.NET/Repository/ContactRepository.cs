f6e910ca7594a46f56f66e4c0d756eb5ea342e2e
J-J-T-M <jonatham.junior.18@gmail.com>
2023-05-18 10:45:34 -0400

    developing list GetAll

---
+++ FirstAppASP.NET/Repository/ContactRepository.cs
@@ -0,0 +1,27 @@
+﻿using FirstAppASP.NET.Data;
+using FirstAppASP.NET.Models;
+
+namespace FirstAppASP.NET.Repository
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly BancoContext _bancoContext;
+
+        public ContactRepository(BancoContext bancoContext) 
+        { 
+            _bancoContext = bancoContext;
+        }
+        public ContactModel Adding(ContactModel contact)
+        {
+            _bancoContext.Contacts.Add(contact);
+            _bancoContext.SaveChanges();
+            
+            return contact;
+        }
+
+        public List<ContactModel> GetAll()
+        {
+            return _bancoContext.Contacts.ToList();
+        }
+    }
+}
