ce36b57a692ed8be429d8b1d9d79ee7d2019eb84
J-J-T-M <jonatham.junior.18@gmail.com>
2023-05-25 09:39:03 -0400

    Revert "creating the contract that updates the contact"
    This reverts commit dfe1dfdd1cdfdb0dc61d7ebda545b5acaf2c2ae2.

--- FirstAppASP.NET/Repository/ContactRepository.cs
+++ FirstAppASP.NET/Repository/ContactRepository.cs
@@ -11,17 +11,6 @@ namespace FirstAppASP.NET.Repository
         { 
             _bancoContext = bancoContext;
         }
-
-        public List<ContactModel> GetAll()
-        {
-            return _bancoContext.Contacts.ToList();
-        }
-
-        public ContactModel GetById(int id)
-        {
-            return _bancoContext.Contacts.FirstOrDefault(x => x.Id == id);
-        }
-
         public ContactModel Adding(ContactModel contact)
         {
             _bancoContext.Contacts.Add(contact);
@@ -30,33 +19,9 @@ namespace FirstAppASP.NET.Repository
             return contact;
         }
 
-        public ContactModel Update(ContactModel contact)
+        public List<ContactModel> GetAll()
         {
-            ContactModel contactDB = GetById(contact.Id);
-
-            if (contactDB == null) throw new Exception("Houve um erro na atualização do contato");
-
-            contactDB.Name = contact.Name;
-            contactDB.Email = contact.Email;
-            contactDB.CellPhone = contact.CellPhone;
-
-            _bancoContext.Contacts.Update(contactDB);
-            _bancoContext.SaveChanges();
-
-            return contactDB;
-
-        }
-
-        public bool Destroy(int id)
-        {
-            ContactModel contactDB = GetById(id);
-
-            if (contactDB == null) throw new Exception("Houve um erro na deleção do contato");
-
-            _bancoContext.Contacts.Remove(contactDB);
-            _bancoContext.SaveChanges();
-            
-            return true;
+            return _bancoContext.Contacts.ToList();
         }
     }
 }
